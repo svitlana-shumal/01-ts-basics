0d0a6a47c4cfdf35cb3b7aad5e6d5f9d6d517e64
Svitlana Shumal <svetlankachajka@gmail.com>
2025-07-15 07:48:44 +0300

    fixed task-2 and task-6

--- src/task-6.ts
+++ src/task-6.ts
@@ -2,6 +2,6 @@ function getFirstElement<T>(arr: T[]): T {
   return arr[0];
 }
 
-console.log(getFirstElement([1, 2, 3])); // 1
-console.log(getFirstElement(["a", "b", "c"])); // "a"
-console.log(getFirstElement([true, false, true])); // true
+console.log(getFirstElement<number>([1, 2, 3])); // 1
+console.log(getFirstElement<string>(["a", "b", "c"])); // "a"
+console.log(getFirstElement<boolean>([true, false, true])); // true
